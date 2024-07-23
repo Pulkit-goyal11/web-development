bf4d1ba46a2088a9e2f637cb452d000ab308b1bb
Pulkit-goyal11 <81236666+Pulkit-goyal11@users.noreply.github.com>
2024-07-23 19:17:41 +0530

    update

---
+++ Learning JS/variables/script.js
@@ -0,0 +1,8 @@
+console.log("code is running")
+console.log("code is also running")
+console.log("code is looking like a wow ")
+var a = 5;
+var b = 6;
+var c="Pulkit"
+console.log(a + b)
+console.log(typeof a, typeof b, typeof c) 
